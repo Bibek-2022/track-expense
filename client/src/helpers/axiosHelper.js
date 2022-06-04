02390653b4d7be0b357de296ec6b97a1d6097529
Bibek-2022 <71386018+Bibek-2022@users.noreply.github.com>
2022-06-04 14:41:44 +1000

    Dashboard and form

--- client/src/helpers/axiosHelper.js
+++ client/src/helpers/axiosHelper.js
@@ -1,13 +1,15 @@
 import axios from "axios";
 
-const apiEp =
+const rootUrl =
   process.env.NODE_ENV === "production"
-    ? "/api/v1/users"
-    : "http://localhost:8000/api/v1/users";
+    ? "/api/v1/"
+    : "http://localhost:8000/api/v1/";
 
+const userEP = rootUrl + "/users";
+const transactionEp = rootUrl + "/transactions";
 export const createUser = async (usrObj) => {
   try {
-    const { data } = await axios.post(apiEp, usrObj);
+    const { data } = await axios.post(userEP, usrObj);
     return data;
   } catch (error) {
     return {
@@ -19,7 +21,21 @@ export const createUser = async (usrObj) => {
 
 export const loginUser = async (usrObj) => {
   try {
-    const { data } = await axios.post(apiEp + "/login", usrObj);
+    const { data } = await axios.post(userEP + "/login", usrObj);
+    return data;
+  } catch (error) {
+    return {
+      status: "error",
+      message: error.message,
+    };
+  }
+};
+
+// Transaction api
+
+export const postTransaction = async (transObj) => {
+  try {
+    const { data } = await axios.post(transactionEp, transObj);
     return data;
   } catch (error) {
     return {
--- client/src/pages/dashboard/Dashboard.js
+++ client/src/pages/dashboard/Dashboard.js
@@ -1,15 +1,80 @@
 import React, { useEffect, useState } from "react";
 import Layout from "../../components/layout/Layout";
 import { Link, useNavigate } from "react-router-dom";
+import { Alert, Button, Col, Form, Row } from "react-bootstrap";
+import { postTransaction } from "../../helpers/axiosHelper";
 
 export const Dashboard = () => {
   const navigation = useNavigate();
-
+  const [form, setForm] = useState({});
+  const [resp, setResp] = useState({});
   useEffect(() => {
     const storedUser = JSON.parse(window.sessionStorage.getItem("user"));
 
     !storedUser?._id && navigation("/");
   }, []);
 
-  return <Layout>Dashboard Layout</Layout>;
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
+  const handleOnSubmit = async (e) => {
+    e.preventDefault();
+    const result = await postTransaction(form);
+    console.log(e);
+    setResp(result);
+  };
+  return (
+    <Layout>
+      <Form className="mt-5" onSubmit={handleOnSubmit}>
+        {resp?.message && (
+          <Alert variant={resp?.status === "success" ? "success" : "danger"}>
+            {" "}
+            {resp?.message}
+          </Alert>
+        )}
+        <Row className="g-2">
+          <Col md="2">
+            <Form.Select name="type" onChange={handleOnChange} required>
+              <option> Choose...</option>
+              <option value="income">Income</option>
+              <option value="expenses">Expense</option>
+            </Form.Select>
+          </Col>
+          <Col md="6">
+            <Form.Control
+              name="title"
+              placeholder="transaction Title"
+              onChange={handleOnChange}
+              required
+            />
+          </Col>
+          <Col md="2">
+            <Form.Control
+              name="amount"
+              placeholder="50"
+              type="number"
+              onChange={handleOnChange}
+              required
+            />
+          </Col>
+          <Col md="2">
+            <Button
+              variant="primary"
+              type="submit"
+              className="form-control"
+              onClick={handleOnSubmit}
+            >
+              Add
+            </Button>
+          </Col>
+        </Row>
+      </Form>
+
+      <hr />
+    </Layout>
+  );
 };
